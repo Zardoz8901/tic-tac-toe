582b7bca4c70e4495ea0d7a74cb839e108cf5bd4
Zardoz8901 <interzoneclearinghouse@gmail.com>
2023-06-16 17:44:00 +0200

    Walk with small steps from the cradle to the CLI

--- brain.ts
+++ brain.ts
@@ -1,7 +1,36 @@
-const game = {
+const gameBoard = {
   gameBoard: [
     [0, 0, 0],
     [0, 0, 0],
     [0, 0, 0],
   ],
 };
+
+const player = () => {
+  const name = prompt("What hail thee far-stranger?");
+  const symbol = prompt(
+    ` Ah ${name} why not choose, Nought or Cross, traveler.`
+  );
+  const order = "Player 1";
+  return { name, symbol, order };
+};
+
+const joe = player();
+
+// function playerSelect() {
+//   const players = {
+//     playerName: prompt("What hail thee far-stranger?"),
+//     playerSign: prompt(`Choose, Nought or Cross, traveler.`),
+//   };
+//   return players;
+// }
+
+// const joe = playerSelect();
+// const shmoe = playerSelect();
+
+// (function game() {
+//   const play = {
+//     init:,
+//   };
+//   return play;
+// })();
