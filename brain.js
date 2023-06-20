b3617a41adcd9dae605157bb0e87d82ac576a5f3
Zardoz8901 <interzoneclearinghouse@gmail.com>
2023-06-20 16:48:57 +0200

    Implement factory function for player select, shades of text adventure

--- brain.js
+++ brain.js
@@ -6,25 +6,37 @@ const gameBoard = {
         [0, 0, 0],
     ],
 };
-const player = () => {
-    const name = prompt("What hail thee far-stranger?");
-    const symbol = prompt(` Ah ${name} why not choose, Nought or Cross, traveler.`);
-    const order = "Player 1";
-    return { name, symbol, order };
+const sanitizeString = (stringIn) => {
+    const string = stringIn.toLowerCase();
+    return string.charAt(0).toUpperCase() + string.slice(1);
 };
-const joe = player();
-// function playerSelect() {
-//   const players = {
-//     playerName: prompt("What hail thee far-stranger?"),
-//     playerSign: prompt(`Choose, Nought or Cross, traveler.`),
-//   };
-//   return players;
-// }
-// const joe = playerSelect();
-// const shmoe = playerSelect();
-// (function game() {
-//   const play = {
-//     init:,
-//   };
-//   return play;
-// })();
+const playerSelect = () => {
+    const player1 = () => {
+        const name = prompt("What hail thee far-stranger?");
+        const promptSymbol = prompt(` Ah ${name}, a name of potential! Now choose. Nought or Cross, traveler?`);
+        const symbol = sanitizeString(promptSymbol);
+        const turnOrder = 1;
+        return { name, symbol, turnOrder };
+    };
+    const player2 = (player1) => {
+        const name = prompt(`And what is thine name, friend of ${player1.name} ?`);
+        const symbolChoice = () => {
+            if (player1.symbol === "Nought") {
+                return "Cross";
+            }
+            else {
+                return "Nought";
+            }
+        };
+        const symbolAlert = alert(`${name}, a name best suited to the ${symbolChoice()}. Now we begin our bloody game!`);
+        const symbol = symbolChoice();
+        const turnOrder = 2;
+        return { name, symbol, turnOrder };
+    };
+    const firstPlayer = player1();
+    const secondPlayer = player2(firstPlayer);
+    return console.log(firstPlayer), console.log(secondPlayer);
+};
+const gameFlow = (() => {
+    playerSelect();
+})();
--- brain.ts
+++ brain.ts
@@ -6,31 +6,42 @@ const gameBoard = {
   ],
 };
 
-const player = () => {
-  const name = prompt("What hail thee far-stranger?");
-  const symbol = prompt(
-    ` Ah ${name} why not choose, Nought or Cross, traveler.`
-  );
-  const order = "Player 1";
-  return { name, symbol, order };
+const sanitizeString = (stringIn: string | null) => {
+  const string = stringIn.toLowerCase();
+  return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
-const joe = player();
+const playerSelect = () => {
+  const player1 = () => {
+    const name = prompt("What hail thee far-stranger?");
+    const promptSymbol = prompt(
+      ` Ah ${name}, a name of potential! Now choose. Nought or Cross, traveler?`
+    );
+    const symbol = sanitizeString(promptSymbol);
+    const turnOrder = 1;
+    return { name, symbol, turnOrder };
+  };
+  const player2 = (player1: any) => {
+    const name = prompt(`And what is thine name, friend of ${player1.name} ?`);
+    const symbolChoice = () => {
+      if (player1.symbol === "Nought") {
+        return "Cross";
+      } else {
+        return "Nought";
+      }
+    };
+    const symbolAlert = alert(
+      `${name}, a name best suited to the ${symbolChoice()}. Now we begin our bloody game!`
+    );
+    const symbol = symbolChoice();
+    const turnOrder = 2;
+    return { name, symbol, turnOrder };
+  };
+  const firstPlayer = player1();
+  const secondPlayer = player2(firstPlayer);
+  return console.log(firstPlayer), console.log(secondPlayer);
+};
 
-// function playerSelect() {
-//   const players = {
-//     playerName: prompt("What hail thee far-stranger?"),
-//     playerSign: prompt(`Choose, Nought or Cross, traveler.`),
-//   };
-//   return players;
-// }
-
-// const joe = playerSelect();
-// const shmoe = playerSelect();
-
-// (function game() {
-//   const play = {
-//     init:,
-//   };
-//   return play;
-// })();
+const gameFlow = (() => {
+  playerSelect();
+})();
