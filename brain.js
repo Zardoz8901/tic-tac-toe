5b33749b8ff76b60952023d75424df6e61973989
Zardoz8901 <interzoneclearinghouse@gmail.com>
2023-08-03 13:23:12 +0200

    Implement current board state, player move tracking, and open board positions

--- brain.js
+++ brain.js
@@ -1,35 +1,56 @@
 "use strict";
 const initGameboard = () => {
-    const gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    return gameBoard;
+    const gameBoard = [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+        [1, 0],
+        [1, 1],
+        [1, 2],
+        [2, 0],
+        [2, 1],
+        [2, 2],
+    ];
+    const playerOnePositions = [];
+    const playerTwoPositions = [];
+    return { gameBoard, playerOnePositions, playerTwoPositions };
 };
 const initMoveTracker = () => {
     const moveTracker = [];
     return moveTracker;
 };
-const playerSelect = () => {
-    const initPlayer = (playerNumber) => {
-        const playOrder = playerNumber;
-        const name = "Player" + playerNumber;
-        const symbol = "X";
-        if (playerNumber === 2) {
-            const symbol = "O";
-            return { name, symbol, playOrder };
-        }
+const initPlayer = (playerNumber) => {
+    const playOrder = playerNumber;
+    const name = "Player" + playerNumber;
+    const symbol = "X";
+    if (playerNumber === 2) {
+        const symbol = "O";
         return { name, symbol, playOrder };
-    };
-    const playerOne = initPlayer(1);
-    const playerTwo = initPlayer(2);
-    return { playerOne, playerTwo };
+    }
+    return { name, symbol, playOrder };
 };
 const insertMove = (gameBoard, symbol, position) => {
-    // const players = playerSelect();
-    // const player1 = players.playerOne;
-    // const player2 = players.playerTwo;
-    // console.log(player1, player2);
-    gameBoard.splice(position, 1, symbol);
-    console.log(gameBoard);
-    return gameBoard;
+    const playerOne = initPlayer(1);
+    const playerTwo = initPlayer(2);
+    const gameState = gameBoard.gameBoard;
+    const playerOnePositions = gameBoard.playerOnePositions;
+    const playerTwoPositions = gameBoard.playerTwoPositions;
+    if (symbol === "X") {
+        playerOnePositions.push(gameState[position]);
+    }
+    else {
+        playerTwoPositions.push(gameState[position]);
+    }
+    gameState.splice(position, 1, symbol);
+    const openPositions = gameState.filter((s) => s != "X" && s != "O");
+    // for (const [key, value] of Object.entries(gameBoard)) {
+    //   console.log(`${key}:${value}`);
+    // }
+    console.log(playerOnePositions);
+    console.log(playerTwoPositions);
+    console.log(gameState);
+    console.log(openPositions);
+    return { gameBoard, openPositions, playerOnePositions };
 };
 const gridSelector = (moveTracker, newGame) => {
     const gridList = document.querySelectorAll("#game-grid> div");
@@ -53,7 +74,5 @@ const gridSelector = (moveTracker, newGame) => {
 const gameFlow = (() => {
     const newGame = initGameboard();
     const moveTracker = initMoveTracker();
-    gridSelector(moveTracker, newGame);
-    // insertMove(newGame, "O", 1);
-    playerSelect();
+    const gridSelect = gridSelector(moveTracker, newGame);
 })();
